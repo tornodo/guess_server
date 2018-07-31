4f614a1b7f984f206ddcfcb64dbd2e77d327f22c
海盗 <guo.bo@sx2017.cn>
2018-07-31 17:58:02 +0800

    Add chat room

---
+++ snake_server/user/ChatRoom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace snake_server.user
+{
+    public class ChatRoom
+    {
+        // 房间最大容量
+        private static readonly int RoomCapacity = 8;
+        private User Owner { set; get; }
+        private LinkedList<User> UserList = new LinkedList<User>();
+
+        public ChatRoom(User user)
+        {
+            this.Owner = user;
+        }
+
+        public bool IsFull()
+        {
+            return this.UserList.Count == RoomCapacity;
+        }
+
+        public bool AddUser(User user)
+        {
+            if (this.IsFull())
+            {
+                return false;
+            }
+
+            UserList.AddLast(user);
+            return true;
+        }
+
+        public bool RemoveUser(User user)
+        {
+            return UserList.Remove(user);
+        }
+    }
+}
--- snake_server/user/User.cs
+++ snake_server/user/User.cs
@@ -4,15 +4,17 @@ namespace snake_server.user
 {
     public class User
     {
-        public User(string key, string name, WebSocket socket)
+        public User(string key, string name, WebSocket socket, int seat)
         {
             this.Key = key;
             this.Name = name;
             this.Socket = socket;
+            this.seat = seat;
             this.Offline = false;
         }
         private string Key { get; }
         private string Name { set; get; }
+        private int seat { set; get; }
         private WebSocket Socket { set; get;}
         private bool Offline { set; get; }
     }
